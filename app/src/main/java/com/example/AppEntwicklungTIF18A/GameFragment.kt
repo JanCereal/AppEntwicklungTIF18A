a2409bd430b7254d872c4cdafa5b579bc97b73f1
JanCereal <46893653+JanCereal@users.noreply.github.com>
2020-11-12 14:25:56 +0100

    GameLoopPrep

--- app/src/main/java/com/example/AppEntwicklungTIF18A/GameFragment.kt
+++ app/src/main/java/com/example/AppEntwicklungTIF18A/GameFragment.kt
@@ -1,11 +1,15 @@
 package com.example.AppEntwicklungTIF18A
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.example.AppEntwicklungTIF18A.databinding.FragmentGameBinding
+import kotlin.random.Random
+
 
 class GameFragment : Fragment() {
     override fun onCreateView(
@@ -13,7 +17,24 @@ class GameFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val binding = FragmentGameBinding.inflate(layoutInflater)
+
+        binding.btnNext.setOnClickListener { view: View ->
+            val rnd = (0..3).random()
+            if (rnd == 1) {
+                binding.imagePanel1.setImageResource(com.example.AppEntwicklungTIF18A.R.drawable.test2)
+                binding.imagePanel2.setImageResource(com.example.AppEntwicklungTIF18A.R.drawable.test2)
+                binding.imagePanel3.setImageResource(com.example.AppEntwicklungTIF18A.R.drawable.test2)
+                binding.imagePanel4.setImageResource(com.example.AppEntwicklungTIF18A.R.drawable.test2)
+            } else {
+                binding.imagePanel1.setImageResource(com.example.AppEntwicklungTIF18A.R.drawable.test)
+                binding.imagePanel2.setImageResource(com.example.AppEntwicklungTIF18A.R.drawable.test)
+                binding.imagePanel3.setImageResource(com.example.AppEntwicklungTIF18A.R.drawable.test)
+                binding.imagePanel4.setImageResource(com.example.AppEntwicklungTIF18A.R.drawable.test)
+            }
+        }
+
         (activity as AppCompatActivity?)!!.supportActionBar!!.show()
-        return null
+        return binding.root
     }
 }
