f05e48712791cb3adca3f6d674f32087506086d0
stellmaj <stellmaj@dhbw-loerrach.de>
2020-11-24 20:21:01 +0100

    I/O Klasse Einfügen einzelner wörter

--- app/src/main/java/com/example/AppEntwicklungTIF18A/IO_updateClass.kt
+++ app/src/main/java/com/example/AppEntwicklungTIF18A/IO_updateClass.kt
@@ -1,6 +1,7 @@
 package com.example.AppEntwicklungTIF18A
 
 import android.content.Context
+import com.beust.klaxon.JsonObject
 import org.json.JSONObject
 import java.io.File
 
@@ -8,29 +9,50 @@ class IO_updateClass {
     companion object {
         var categoryCollection = ArrayList<Pair<String, MutableList<String>>>()
 
-        fun updateCategory(context: Context?, changedCategory: ArrayList<String>) {
-            writeCategoryJson(context, changedCategory)
+        fun addSingleCategoryWord(context: Context?, categoryName: String, addedWord: String) {
+            var data = readCategoryJson(context)
+            val oldValues = data?.get(categoryName)?.toString() + "," + addedWord
+            data?.put(categoryName, oldValues)
+            writeJsonData(context, data)
         }
 
+        fun deleteSingleCategoryWord(context: Context, categoryName: String, deletedWord: String) {
 
-        fun updateSavedFile(context: Context?, toSave: ArrayList<Pair<String,MutableList<String>>>) {
+        }
+
+        fun updateSavedFile(
+            context: Context?,
+            toSave: ArrayList<Pair<String, MutableList<String>>>
+        ) {
             //TODO update die hinterlegte File?
+
         }
 
-        fun getSavedFile(context: Context?): ArrayList<Pair<String,MutableList<String>>> {
+        fun getSavedFile(context: Context?): ArrayList<Pair<String, MutableList<String>>> {
             val data = readCategoryJson(context)
             categoryCollection.clear()
-            data?.keys()?.forEach {categoryName ->
+            data?.keys()?.forEach { categoryName ->
                 var words = ""
                 data[categoryName].let { any -> words += any }
                 words = words.replace(Regex("""[$\[\]\"]"""), "")
                 val listWords = words.split(',')
-                categoryCollection.add(Pair(categoryName,listWords as MutableList<String>))
+                categoryCollection.add(Pair(categoryName, listWords as MutableList<String>))
             }
-           return categoryCollection
+            return categoryCollection
         }
 
-        private fun writeCategoryJson(context: Context?, changedCategory: ArrayList<String>) {
+        private fun writeJsonData(context: Context?, data: JSONObject?) {
+            try {
+                val fileName = "categoryData.json"
+                context?.openFileOutput(fileName, Context.MODE_PRIVATE).use { output ->
+                    output?.write(data.toString().toByteArray())
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        fun writeCategoryJson(context: Context?) {
             //TODO convert was auch immer wir für ne liste benutzen zu Map
             var categoryMap: MutableMap<String, MutableList<String>> = mutableMapOf()
             categoryMap["Nature"] = mutableListOf("water", "tree", "dirt", "flower", "bird")
