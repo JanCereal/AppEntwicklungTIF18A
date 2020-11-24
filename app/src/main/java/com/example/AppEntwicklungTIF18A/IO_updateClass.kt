e3023667d12335c8fb1ee74ca29680fb82922861
stellmaj <stellmaj@dhbw-loerrach.de>
2020-11-24 18:08:38 +0100

    I/O Klasse Anpassen der Liste an ArrayList<string,list<string>>

--- app/src/main/java/com/example/AppEntwicklungTIF18A/IO_updateClass.kt
+++ app/src/main/java/com/example/AppEntwicklungTIF18A/IO_updateClass.kt
@@ -6,18 +6,18 @@ import java.io.File
 
 class IO_updateClass {
     companion object {
-        var categoryCollection = ArrayList<Category>()
+        var categoryCollection = ArrayList<Pair<String, MutableList<String>>>()
 
         fun updateCategory(context: Context?, changedCategory: ArrayList<String>) {
             writeCategoryJson(context, changedCategory)
         }
 
 
-        fun updateSavedFile(context: Context?, toSave: ArrayList<Category>) {
+        fun updateSavedFile(context: Context?, toSave: ArrayList<Pair<String,MutableList<String>>>) {
             //TODO update die hinterlegte File?
         }
 
-        fun getSavedFile(context: Context?): ArrayList<Category> {
+        fun getSavedFile(context: Context?): ArrayList<Pair<String,MutableList<String>>> {
             val data = readCategoryJson(context)
             categoryCollection.clear()
             data?.keys()?.forEach {categoryName ->
@@ -25,14 +25,9 @@ class IO_updateClass {
                 data[categoryName].let { any -> words += any }
                 words = words.replace(Regex("""[$\[\]\"]"""), "")
                 val listWords = words.split(',')
-                val newCategory = Category(R.drawable.ic_android, categoryName, listWords as MutableList<String>)
-                categoryCollection.add(newCategory)
+                categoryCollection.add(Pair(categoryName,listWords as MutableList<String>))
             }
-            categoryCollection.forEach {
-                println(it._categoryName)
-                println(it._categoryMembers)
-            }
-            return categoryCollection
+           return categoryCollection
         }
 
         private fun writeCategoryJson(context: Context?, changedCategory: ArrayList<String>) {
@@ -40,15 +35,12 @@ class IO_updateClass {
             var categoryMap: MutableMap<String, MutableList<String>> = mutableMapOf()
             categoryMap["Nature"] = mutableListOf("water", "tree", "dirt", "flower", "bird")
 
-
-            val file = File(context?.filesDir, "categoryData")
-
-            var jsonString = JSONObject(categoryMap as Map<String, MutableList<String>>).toString()
+            var jsonObject = JSONObject(categoryMap as Map<String, MutableList<String>>).toString()
 
             try {
                 val fileName = "categoryData.json"
                 context?.openFileOutput(fileName, Context.MODE_PRIVATE).use { output ->
-                    output?.write(jsonString.toByteArray())
+                    output?.write(jsonObject.toByteArray())
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
