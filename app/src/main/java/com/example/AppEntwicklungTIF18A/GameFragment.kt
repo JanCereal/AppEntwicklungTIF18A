b2b019e8a95ee7ef8a1dc8f2700bf72a11e6c997
JanCereal <46893653+JanCereal@users.noreply.github.com>
2020-12-17 11:34:44 +0100

    Cleanup / default Images / emptysearchBug

--- app/src/main/java/com/example/AppEntwicklungTIF18A/GameFragment.kt
+++ app/src/main/java/com/example/AppEntwicklungTIF18A/GameFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
@@ -24,154 +25,182 @@ import kotlin.random.Random
 import kotlin.random.nextInt
 
 class GameFragment : Fragment() {
-    private var mistakes = 0
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val inputManager =
-            context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        val binding = FragmentGameBinding.inflate(layoutInflater)
-        val tempKeywordList = arguments?.getStringArrayList("selectedCategory")
-        mistakes = arguments?.getInt("Mistakes") as Int
+   private var mistakes = 0
+   override fun onCreateView(
+      inflater: LayoutInflater,
+      container: ViewGroup?,
+      savedInstanceState: Bundle?
+   ): View? {
+      val inputManager =
+         context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+      val binding = FragmentGameBinding.inflate(layoutInflater)
+      val tempKeywordList = arguments?.getStringArrayList("selectedCategory")
+      mistakes = arguments?.getInt("Mistakes") as Int
 
-        //region search
-        if (tempKeywordList?.size != null) {
-            tempKeywordList.shuffle()
+      //region search
+      if (tempKeywordList?.size != null) {
+         tempKeywordList.shuffle()
+         parseSearchJSON(binding, tempKeywordList[0])
+
+         //region display Hint/Mistakes
+         val sharedPrefHint =
+            activity?.getSharedPreferences(getString(R.string.wordHint), Context.MODE_PRIVATE)
+         if (sharedPrefHint != null && sharedPrefHint.getString(R.string.wordHint.toString(), "")
+               .equals("1")
+         ) {
+            binding.charCountTextView.text =
+               "Hint: " + tempKeywordList[0].toCharArray().size + " Letter"
+         }
+         //SetMistakes
+         binding.MistakeCountTextView.text = "Mistakes: $mistakes"
+         //endregion
+
+      } else {
+         println("rKeywordList empty")
+      }
+      //endregion
+
+      // region Buttons and EnterKey (UserAnswerAction)
+      binding.answerTextView.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+         if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+            checkAnswer(tempKeywordList as ArrayList<String>, v, inputManager)
+            return@OnKeyListener true
+         }
+         false
+      })
+      binding.btnAnswer.setOnClickListener { view: View ->
+         if (tempKeywordList?.size != 0) {
+            checkAnswer(tempKeywordList as ArrayList<String>, view, inputManager)
+         }
+      }
+
+      binding.btnReroll.setOnClickListener { view: View ->
+         if (tempKeywordList != null) {
             parseSearchJSON(binding, tempKeywordList[0])
+         }
+      }
+      //endregion
 
-            //region display Hint/Mistakes
-            val sharedPrefHint = activity?.getSharedPreferences(getString(R.string.wordHint), Context.MODE_PRIVATE)
-            if (sharedPrefHint != null && sharedPrefHint.getString(R.string.wordHint.toString(), "").equals("1")) {
-                binding.charCountTextView.text = "Hint: " + tempKeywordList[0].toCharArray().size + " Letter"
+      (activity as AppCompatActivity?)!!.supportActionBar!!.show()
+      return binding.root
+   }
+
+   /**
+    * Sucht vier zufälliger Bilder zum Suchbegriff über die PixaBayAPI und stellt diese da
+    */
+   private fun parseSearchJSON(binding: FragmentGameBinding, keyword: String) {
+      val url =
+         "https://pixabay.com/api/?key=5303976-fd6581ad4ac165d1b75cc15b3&q=$keyword&image_type=photo&pretty=true"
+      val imageUrls = mutableListOf("1", "2", "3", "4")
+      val requestQueue: RequestQueue? = Volley.newRequestQueue(context)
+
+      val request =
+         JsonObjectRequest(Request.Method.GET, url, null, { response ->
+            try {
+               val jsonArray = response.getJSONArray("hits")
+               val randArray = mutableListOf<Int>()
+               var done = 0
+
+               //get Random Unique Images from API
+               while (true) {
+                  if (jsonArray.length() != 0) {
+                     val tempRand = Random.nextInt(0 until jsonArray.length())
+                     if (isUnique(randArray, tempRand)) {
+                        randArray.add(done, tempRand)
+                        done++
+                        if (done == 4) {
+                           break
+                        }
+                     }
+
+                  } else {
+                     Thread.sleep(1_000)
+                     Toast.makeText(context, "No images found!", Toast.LENGTH_LONG).show()
+                     view?.findNavController()?.navigateUp()
+                     break
+                  }
+               }
+
+               //displays Images
+               if(!randArray.isEmpty()) {
+                  for (i in 0 until 4) {
+                     val employee = jsonArray.getJSONObject(randArray[i])
+                     val imageUrl = employee.getString("previewURL")
+                     imageUrls.add(i, imageUrl)
+                     when (i) {
+                        0 -> Picasso.get().load(imageUrls[i]).into(imagePanel1)
+                        1 -> Picasso.get().load(imageUrls[i]).into(imagePanel2)
+                        2 -> Picasso.get().load(imageUrls[i]).into(imagePanel3)
+                        3 -> Picasso.get().load(imageUrls[i]).into(imagePanel4)
+                        else -> {
+                           print("error")
+                        }
+                     }
+                  }
+               }
+            } catch (e: JSONException) {
+               e.printStackTrace()
             }
-            //SetMistakes
-            binding.MistakeCountTextView.text = "Mistakes: $mistakes"
+         }, { error -> error.printStackTrace() })
+      requestQueue?.add(request)
+   }
+
+   /**
+    * Überprüft die vom Nutzer gegebene Antwort
+    */
+   private fun checkAnswer(
+      tempKeywordList: ArrayList<String>,
+      v: View,
+      inputManager: InputMethodManager
+   ) {
+      val userAnswer = answerTextView.text.toString()
+      if (tempKeywordList.size != 0) {
+         // Richtige Antwort
+         if (userAnswer.trim().equals(tempKeywordList[0], true)) {
+
+            val bundle = bundleOf(
+               "selectedCategory" to tempKeywordList,
+               "categoryName" to arguments?.getString("categoryName"),
+               "Mistakes" to mistakes
+            )
+            tempKeywordList.removeAt(0)
+            view?.findNavController()?.navigate(R.id.action_gameFragment_to_successFragment, bundle)
+
+            //region soundCheck
+            val sharedPrefHint =
+               activity?.getSharedPreferences(getString(R.string.wordHint), Context.MODE_PRIVATE)
+            if (sharedPrefHint != null && sharedPrefHint.getString(R.string.sounds.toString(), "")
+                  .equals("1")
+            ) {
+               val soundBox = MediaPlayer.create(this.context, R.raw.ding)
+               soundBox.start()
+            }
             //endregion
 
-        } else {
-            println("rKeywordList empty")
-        }
-        //endregion
+            //SetMistakes
+            MistakeCountTextView.text = "Fehler: $mistakes"
 
-        // region Buttons and EnterKey (UserAnswerAction)
-        binding.answerTextView.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
-            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
-                checkAnswer(tempKeywordList as ArrayList<String>, v, inputManager)
-                return@OnKeyListener true
-            }
-            false
-        })
-        binding.btnAnswer.setOnClickListener { view: View ->
-            if (tempKeywordList?.size != 0) {
-                checkAnswer(tempKeywordList as ArrayList<String>, view, inputManager)
-            }
-        }
+         } else {
+            mistakes++
+            answerTextView.hint = "Wrong!"
+            answerTextView.setText("")
+            //SetMistakes
+            this.MistakeCountTextView.text = "Fehler: $mistakes"
+         }
+      }
+      inputManager.hideSoftInputFromWindow(v.windowToken, 0)
+   }
 
-        binding.btnReroll.setOnClickListener { view: View ->
-            if (tempKeywordList != null) {
-                parseSearchJSON(binding, tempKeywordList[0])
-            }
-        }
-        //endregion
-
-        (activity as AppCompatActivity?)!!.supportActionBar!!.show()
-        return binding.root
-    }
-
-    /**
-     * Sucht vier zufälliger Bilder zum Suchbegriff über die PixaBayAPI und stellt diese da
-     */
-    private fun parseSearchJSON(binding: FragmentGameBinding, keyword: String) {
-        val url =
-            "https://pixabay.com/api/?key=5303976-fd6581ad4ac165d1b75cc15b3&q=$keyword&image_type=photo&pretty=true"
-        val imageUrls = mutableListOf("1", "2", "3", "4")
-        val requestQueue: RequestQueue? = Volley.newRequestQueue(context)
-
-        val request =
-            JsonObjectRequest(Request.Method.GET, url, null, { response ->
-                try {
-                    val jsonArray = response.getJSONArray("hits")
-                    val randArray = mutableListOf<Int>()
-                    var done = 0
-                    while (true) {
-                        val tempRand = Random.nextInt(0 until jsonArray.length())
-                        if (isUnique(randArray, tempRand)) {
-                            randArray.add(done, tempRand)
-                            done++
-                            if (done == 4) {
-                                break
-                            }
-                        }
-                    }
-
-                    for (i in 0 until 4) {
-                        val employee = jsonArray.getJSONObject(randArray[i])
-                        val imageUrl = employee.getString("previewURL")
-                        imageUrls.add(i, imageUrl)
-                        when (i) {
-                            0 -> Picasso.get().load(imageUrls[i]).into(imagePanel1)
-                            1 -> Picasso.get().load(imageUrls[i]).into(imagePanel2)
-                            2 -> Picasso.get().load(imageUrls[i]).into(imagePanel3)
-                            3 -> Picasso.get().load(imageUrls[i]).into(imagePanel4)
-                            else -> {
-                                print("error")
-                            }
-                        }
-                    }
-                } catch (e: JSONException) {
-                    e.printStackTrace()
-                }
-            }, { error -> error.printStackTrace() })
-        requestQueue?.add(request)
-    }
-
-    /**
-     * Überprüft die vom Nutzer gegebene Antwort
-     */
-    private fun checkAnswer(tempKeywordList: ArrayList<String>, v: View, inputManager: InputMethodManager) {
-        val userAnswer = answerTextView.text.toString()
-        if (tempKeywordList.size != 0) {
-            // Richtige Antwort
-            if (userAnswer.trim().equals(tempKeywordList[0], true)) {
-               
-                val bundle = bundleOf("selectedCategory" to tempKeywordList,"categoryName" to arguments?.getString("categoryName"),  "Mistakes" to mistakes)
-                tempKeywordList.removeAt(0)
-                view?.findNavController()?.navigate(R.id.action_gameFragment_to_successFragment, bundle)
-
-                //region soundCheck
-                val sharedPrefHint = activity?.getSharedPreferences(getString(R.string.wordHint), Context.MODE_PRIVATE)
-                if (sharedPrefHint != null && sharedPrefHint.getString(R.string.sounds.toString(), "").equals("1")) {
-                    val soundBox = MediaPlayer.create(this.context, R.raw.ding)
-                    soundBox.start()
-                }
-                //endregion
-
-                //SetMistakes
-                MistakeCountTextView.text = "Fehler: $mistakes"
-
-            } else {
-                mistakes++
-                answerTextView.hint = "Wrong!"
-                answerTextView.setText("")
-                //SetMistakes
-                this.MistakeCountTextView.text = "Fehler: $mistakes"
-            }
-        }
-        inputManager.hideSoftInputFromWindow(v.windowToken, 0)
-    }
-
-    /**
-     * Überprüft die einzigartigkeit eines Bilders in einer gegebenen Liste von Bildern
-     */
-    private fun isUnique(list: MutableList<Int>, toBeChecked: Int): Boolean {
-        for (i in 0 until list.size) {
-            if (list[i] == toBeChecked) {
-                return false
-            }
-        }
-        return true
-    }
+   /**
+    * Überprüft die einzigartigkeit eines Bilders in einer gegebenen Liste von Bildern
+    */
+   private fun isUnique(list: MutableList<Int>, toBeChecked: Int): Boolean {
+      for (i in 0 until list.size) {
+         if (list[i] == toBeChecked) {
+            return false
+         }
+      }
+      return true
+   }
 }
 
